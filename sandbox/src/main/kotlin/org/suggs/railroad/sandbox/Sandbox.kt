3804065af164a41f0450e935e93792d61541f665
Peter Suggitt <me@suggs.org.uk>
2025-10-07 07:55:01 +0100

    wsdl now generating the class files for the jar

--- sandbox/src/main/kotlin/org/suggs/railroad/sandbox/Sandbox.kt
+++ sandbox/src/main/kotlin/org/suggs/railroad/sandbox/Sandbox.kt
@@ -1,11 +1,14 @@
 package org.suggs.railroad.sandbox
 
+import uk.co.nationalrail.opendbws.AccessToken
+import uk.co.nationalrail.opendbws.Ldb
+
 class Sandbox {
     companion object {
         fun testConnectionWithToken(apiToken: String) {
-//            val soapService = Ldb().ldbServiceSoap
-//            val token = AccessToken()
-//            token.tokenValue = apiToken
+            val soapService = Ldb().ldbServiceSoap
+            val token = AccessToken()
+            token.tokenValue = apiToken
 
 //            var board = soapService.getDepartureBoard()
         }
