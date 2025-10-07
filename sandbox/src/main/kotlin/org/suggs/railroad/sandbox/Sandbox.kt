b7cebcce3435ea233ec639e0314644192f37e466
Peter Suggitt <me@suggs.org.uk>
2025-10-07 08:07:35 +0100

    updated wsdl build!

--- sandbox/src/main/kotlin/org/suggs/railroad/sandbox/Sandbox.kt
+++ sandbox/src/main/kotlin/org/suggs/railroad/sandbox/Sandbox.kt
@@ -6,9 +6,9 @@ import uk.co.nationalrail.opendbws.Ldb
 class Sandbox {
     companion object {
         fun testConnectionWithToken(apiToken: String) {
-            val soapService = Ldb().ldbServiceSoap
-            val token = AccessToken()
-            token.tokenValue = apiToken
+//            val soapService = Ldb().ldbServiceSoap
+//            val token = AccessToken()
+//            token.tokenValue = apiToken
 
 //            var board = soapService.getDepartureBoard()
         }
