72f6bbd698c94ccd20d30166b1338d82d3eec13c
Peter Suggitt <me@suggs.org.uk>
2025-10-07 08:21:13 +0100

    updated build for ws

--- sandbox/src/main/kotlin/org/suggs/railroad/sandbox/Sandbox.kt
+++ sandbox/src/main/kotlin/org/suggs/railroad/sandbox/Sandbox.kt
@@ -1,14 +1,19 @@
 package org.suggs.railroad.sandbox
 
+import org.slf4j.LoggerFactory
 import uk.co.nationalrail.opendbws.AccessToken
 import uk.co.nationalrail.opendbws.Ldb
 
 class Sandbox {
     companion object {
+
+        private val log = LoggerFactory.getLogger(this::class.java)
+
         fun testConnectionWithToken(apiToken: String) {
-//            val soapService = Ldb().ldbServiceSoap
-//            val token = AccessToken()
-//            token.tokenValue = apiToken
+            log.info("Using token $apiToken")
+            val soapService = Ldb().ldbServiceSoap
+            val token = AccessToken()
+            token.tokenValue = apiToken
 
 //            var board = soapService.getDepartureBoard()
         }
